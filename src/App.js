b4055bc019866bd5e95fc8c0c2269f7b0ac2029e
jamil-codemen <jamil@codemen.net>
2021-07-28 12:06:04 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Hello from "./pages/Hello";
+import Bye from "./pages/Bye";
+import Navbar from './components/Navbar';
 function App() {
   return (
+    <Router>
+      <Navbar />
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+      <Route exact path="/"  component={Hello}/>
+  <Route exact path="/bye"  component={Bye}/>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Hello.js
@@ -0,0 +1,7 @@
+export default function Hello (){
+    return(
+        <div className="text-red-500">
+            hello boy
+        </div>
+    )
+}
